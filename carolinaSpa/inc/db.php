1840b7d617b66523627c0bf837bce9ad4c11ef00
= <=>
2021-03-10 21:48:58 +0100

    Updates

--- carolinaSpa/inc/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	//$db = new mysqli('localhost','root','','carolina_spa');
-	$db = new mysqli('sql11.freemysqlhosting.net','sql11228321','Bpxfhv9ZGc','sql11228321');
-	if ($db->connect_error) {
-		$error = $db->connect_error;
-	}
-
-?>
